82b6a86a344f77407d1379f14907db3ee351cef8
BThomas <bran_thom@yahoo.com>
2023-11-17 00:17:19 -0600

    alternating caps

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("connected");
+var input = document.getElementById("test-input");
+var output = document.getElementById("test-output");
+
+input.addEventListener("change", (event) => {
+    string_sentence = event.target.value;
+    console.log(string(string_sentence));
+    output.innertext = string(string_sentence);
+});
+
+function string(s) {
+    let newString = "";
+    for (let letter = 0; letter < s.length; letter++) {
+        if (letter % 2 === 0) {
+            newString += s[letter].toUpperCase();
+        } else {
+            newString += s[letter].toLowerCase();
+        }
+    }
+
+    output.innerText = newString;
+    return newString;
+}
